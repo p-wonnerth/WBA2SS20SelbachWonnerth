ca0d8461f171721ec66bfe1ac8dbd495703827ce
p-wonnerth <philipp_wonnerth@hotmail.de>
2020-06-17 20:17:50 +0200

    Songs im Client implementiert

--- SongWish/client.js
+++ SongWish/client.js
@@ -3,9 +3,9 @@ const fetch = require('node-fetch')
 
 const Url = 'http://localhost:3000/'
 const djId = ""
+const songId = "5eea3a8c1177ec0f28b4bf24"
 
-//DJ-Funtionen
-
+//DJ-Funktionen
 //Dj erstellen
 async function postDj(url = Url, data = {}) {
     const res = await fetch(url, {
@@ -39,28 +39,98 @@ async function deleteDj(url = Url, id = djId) {
     return res.json();
 }
 
-//Aufruf der verschiedener  DJ-Funktionen
-
-postDj(Url + 'songs', { title: "Grenade", artist: "Bruno Mars"})
+//Aufruf der verschiedenen  DJ-Funktionen
+/* postDj(Url + 'dj', { name: "Happ", vorname: "Kevin", kuenstlerName: "Happy"})
 .then(data => {
     console.log(data)
-});
+}); */
 
 /*getDj();
-
 getOneDj(Url + 'dj/' + djId,  )
 .then(id => {
     console.log(id)
-});
+});*/
 
-deleteDj(Url + 'dj/' + djId, )
+/*deleteDj(Url + 'dj/' + djId, )
 .then(id => {
     console.log(id)
 })*/
 
-//Song-Funktionen
+/* getDj() */
 
+//Song-Funktionen
 //Song erstellen
+async function postSong(url = Url, data = {}) {
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(data)
+    })
+    return res.json();
+}
+
+//Alle Songs
+async function getSongs() {
+    fetch(Url + 'songs')
+.then(data => {return data.json()})
+.then(res=>{console.log(res)})
+}
+
+//Einen Song
+async function getOneSong(url = Url, id = songId) {
+    const res = await fetch(url)
+    return res.json();
+}
+
+//Song löschen
+async function deleteSong(url = Url, id = songId) {
+    const res = await fetch(url, {
+        method: 'DELETE'
+    })
+    return res.json();
+}
+
+//Song verändern
+async function changeSong(url = Url, data = {}) {
+    const res = await fetch(Url + 'songs/' + songId, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    return res.json();
+}
+
+//Aufruf der verschiedenen Song-Funktionen
+/* postSong(Url + 'songs', { title: "Mockingbird", artist: "Eminem"})
+.then(data => {
+    console.log(data)
+}); */
+
+/* getSongs() */
+
+/* getOneSong(Url + 'songs/' + songId,  )
+.then(id => {
+    console.log(id)
+}); */
+
+/* deleteSong(Url + 'songs/' + songId, )
+.then(id => {
+    console.log(id)
+}) */
+
+/* changeSong(Url + 'songs/' + songId, { title: "Mockingbird", artist: "Eminem"})
+.then(data => {
+    console.log(data)
+}); */
+
+
+
+
+
 
 
 
